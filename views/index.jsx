35b70fbeff86e833d3628be84ec679202246a69d
Kabirdas Henry <kbrdsk@gmail.com>
2020-09-22 13:07:19 -0700

    new message button

--- views/index.jsx
+++ views/index.jsx
@@ -1,11 +1,24 @@
 const React = require("react");
 
 function Index(props) {
-	return <div>{props.messages.map(renderMessage)}</div>;
+	return (
+		<div>
+			{props.messages.map(renderMessage)}
+			<NewMessage />
+		</div>
+	);
 }
 
 function renderMessage(message) {
 	return <div>{`${message.added} ${message.user}: ${message.text}`}</div>;
+}
+
+function NewMessage(props) {
+	return (
+		<form action="/new">
+			<input type="submit" name="newMessage" value="New Message"/>
+		</form>
+	);
 }
 
 module.exports = Index;
