4989a65d48ee80c5fb27aab1ab22512a96712d30
Kabirdas Henry <kbrdsk@gmail.com>
2020-09-22 12:32:03 -0700

    added react views

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const newRouter = require('./routes/new');
 
-var app = express();
+const app = express();
+
+app.set("views", __dirname + "views");
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -15,6 +19,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/new', newRouter);
 
 module.exports = app;
---
+++ views/index.jsx
@@ -0,0 +1,8 @@
+const React = require("react");
+
+
+function Index(props){
+	return <div>Messages go here!</div>
+}
+
+modules.exports = Index;
